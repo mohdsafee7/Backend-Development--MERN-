c8b360d4b6d669492d19d5c8a68ddf85e4f0ed36
Mohd Safee <2k23.cs2313689@gmail.com>
2026-03-04 07:11:01 +0530

    MVC Architecture

---
+++ ExpressJs/05-MVC-Architecture/models/books.js
@@ -0,0 +1,7 @@
+//named export
+exports.BOOK = [
+  {id : 1, title : "Pirates of the Carribean", author : "Jack Sparrow"},
+  {id : 2, title : "In Search of Gold", author : "Mohd Safee"},
+  {id : 3, title : "In Search of Diamond", author : "Mohd Faiz"},
+  {id : 4, title : "In Search of Tamba", author : "Pappu Bhaiya"}
+];
