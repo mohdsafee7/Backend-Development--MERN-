bf976c329efb5d2d9baaa3f10fd6bc6e5a516a8d
Mohd Safee <2k23.cs2313689@gmail.com>
2026-04-15 10:57:58 +0530

    Signup Route

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 
+app.use(express.json());
+
 app.get('/', (req, res)=>{
   console.log('everything is fine');
 
---
+++ routes/user.routes.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { db } from '../db/index.js';
+import { usersTable } from '../models/index.js';
+import { eq } from 'drizzle-orm';
+import { randomBytes, createHmac } from 'crypto';
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
+
+  // if(!firstname)    return res.status(400).json({error: 'firstname is requires'}); 
+  // we are going to use Zod for validating these fields
+
+  const [existingUser] = await db.select({
+    id: usersTable.id,
+  }).from(usersTable).where(eq(usersTable.email, email));
+
+  //if user exist
+  if(existingUser)
+      return res
+        .status(400)
+        .json({ error: `User with email ${email} already exists!`});
+  
+  const salt = randomBytes(256).toString('hex');
+  const hashedPassword = createHmac('sha256', salt).update(password).digest('hex');
+
+
+  const [user] = await db.insert(usersTable).values({
+    email,
+    firstname,
+    lastname,
+    salt,
+    password: hashedPassword,
+  }).returning({ id: usersTable.id });
+
+
+  return res.status(201).json({ data: { userId: user.id } } );
+})
+
+export default router;
